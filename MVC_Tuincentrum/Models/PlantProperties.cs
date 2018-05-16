86484584c9f572fe41d28b4760b47f84a7cacd4b
Pieter-1337 <pieterbracke@msn.com>
2018-05-16 11:58:40 +0200

    Add project files.

---
+++ MVC_Tuincentrum/Models/PlantProperties.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace MVC_Tuincentrum.Models
+{
+    public class PlantProperties
+    {
+        [Range(0, 1000)]
+        [HiddenInput()]
+        public decimal VerkoopPrijs { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string Kleur { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int Levnr { get; set; }
+
+        [ScaffoldColumn(false)]
+        public Leverancier Leverancier { get; set; }
+    }
+}
---
+++ MVC_Tuincentrum/Services/SoortService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVC_Tuincentrum.Models;
+
+namespace MVC_Tuincentrum.Services
+{
+    public class SoortService
+    {
+        public List<Soort> FindByNaam(string Naam)
+        {
+            using(var db = new MVCTuinCentrumEntities())
+            {
+                var query = from soort in db.Soorts where soort.Naam.Contains(Naam) orderby soort.Naam select soort;
+                var soorten = query.ToList();
+                return soorten;
+            }          
+        }
+    }
+}
